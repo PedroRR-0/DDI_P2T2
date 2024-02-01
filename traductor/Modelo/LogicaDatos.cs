899991196d31f391a028dc18249d8d7caa0e8277
PedroRR-0 <prmorfeo@gmail.com>
2024-02-01 10:58:14 +0100

    empezamos logica datos

---
+++ traductor/Modelo/LogicaDatos.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace traductor.Modelo
+{
+    public class LogicaDatos
+    {
+        private ConcurrentDictionary<string, string> parejaPalabra = new ConcurrentDictionary<string, string>();
+        private static LogicaDatos? instancia;
+
+        public bool anadirPalabra(string palabraIng, string palabraEsp)
+        {
+            bool correcto = parejaPalabra.TryAdd(palabraIng, palabraEsp);
+            return correcto;
+        }
+
+        public static LogicaDatos getInstance()
+        {
+            instancia??=new LogicaDatos();
+            return instancia;
+        }
+        
+    }
+}
--- traductor/VistaTabs/TabListado.xaml.cs
+++ traductor/VistaTabs/TabListado.xaml.cs
@@ -12,18 +12,32 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using traductor.Modelo;
 
 namespace traductor
 {
     public partial class Listado : UserControl
     {
+        private LogicaDatos logica;
         public Listado() { 
             InitializeComponent();
+            logica = LogicaDatos.getInstance();
         }
 
         private void btnAnadListado_Click(object sender, RoutedEventArgs e)
         {
-
+            string ing = campoIngListado.Text.Trim();
+            ing = char.ToUpper(ing[0])+ing.Substring(1);
+            string esp = campoEspListado.Text.Trim();
+            esp = char.ToUpper(esp[0]) + esp.Substring(1);
+            if(logica.anadirPalabra(ing, esp))
+            {
+                campoIngListado.Clear();
+                campoEspListado.Clear();
+            } else
+            {
+                btnAnadListado.Content = "ya existe";
+            }
         }
     }
 }
