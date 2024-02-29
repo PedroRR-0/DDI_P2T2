9291fc25a91b78593a68776d122419ed2fee0442
PedroRR-0 <prmorfeo@gmail.com>
2024-02-29 09:41:05 +0100

    act datos

--- aeropuerto/aeropuerto/Vista/Pasajeros.xaml.cs
+++ aeropuerto/aeropuerto/Vista/Pasajeros.xaml.cs
@@ -2,6 +2,8 @@
 using MySqlConnector;
 using System;
 using System.Collections.Generic;
+using System.Data.Common;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -26,7 +28,39 @@ namespace aeropuerto
         public Pasajeros()
         {
             InitializeComponent();
+            cargarPasajeros();
 
         }
+
+        private void cargarPasajeros()
+        {
+            try
+            {
+                MySqlConnection c = conex.obtenerConexion();
+
+                // Crear el comando SQL
+                string consulta = "SELECT * FROM pasajeros";
+                using (DbCommand comando = new MySqlCommand(consulta, c))
+                {
+                    // Crear el adaptador de datos y llenar el DataTable
+                    using (DbDataAdapter adaptador = new MySqlDataAdapter((MySqlCommand)comando))
+                    {
+                        DataTable dataTable = new DataTable();
+                        adaptador.Fill(dataTable);
+
+                        //Limpiar la fuente de datos
+                        tablaPasaj.ItemsSource = null;
+                        // Asignar el DataTable como origen de datos para el DataGrid                         
+                        tablaPasaj.ItemsSource = dataTable.DefaultView;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al cargar datos desde la base de datos: " + ex.Message);
+            }
+        }
     }
+
+
 }
